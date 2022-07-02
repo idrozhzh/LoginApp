0bbb56b0eb9391fc3803f5de4f00f3f66c58b442
i_drozhzh <idrozhzh@gmail.com>
2022-07-02 16:12:30 +0300

    Launch screen setup and added base classes files

---
+++ LohinApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Иван Дрожжин on 02.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
